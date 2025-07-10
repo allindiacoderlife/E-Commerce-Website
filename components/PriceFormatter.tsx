1b10f780302560340872906822d4914b33ba3e1a
Allindiacoderlife <chiragsaxena728@gmail.com>
2025-07-10 21:07:24 +0530

    price fix

--- components/PriceFormatter.tsx
+++ components/PriceFormatter.tsx
@@ -6,11 +6,12 @@ interface Props {
 }
 
 const PriceFormatter = ({ amount, className }: Props) => {
-  const formattedPrice = new Number(amount).toLocaleString("en-US", {
-    currency: "USD",
-    style: "currency",
-    minimumFractionDigits: 2,
-  });
+  const formattedPrice = new Number(amount).toLocaleString("en-IN", {
+  style: "currency",
+  currency: "INR",
+  minimumFractionDigits: 2,
+});
+
   return (
     <span
       className={twMerge("text-sm font-semibold text-darkColor", className)}
