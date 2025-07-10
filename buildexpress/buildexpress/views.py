41710c01004d81add54219d7cf4648a0ae83b8d9
Allindiacoderlife <chiragsaxena728@gmail.com>
2025-07-10 20:33:29 +0530

    new  update

--- buildexpress/buildexpress/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def navbar(request):
-    return render(request, 'navbar.html')
